5715568c0afad802703d0163b0dcae43bd0a26fa
BonuRakesh <112648987+BonuRakesh@users.noreply.github.com>
2022-10-08 11:41:43 +0530

    Add files via upload

---
+++ No. of perfect squares 5.3.py
@@ -0,0 +1,20 @@
+
+def getMinSquares(n):
+
+	if n <= 3:
+		return n;
+
+	res = n
+
+	for x in range(1, n + 1):
+		temp = x * x;
+		if temp > n:
+			break
+		else:
+			res = min(res, 1 + getMinSquares(n
+								- temp))
+	
+	return res;
+s= input("enter the number : ")
+print(getMinSquares(4))
+
